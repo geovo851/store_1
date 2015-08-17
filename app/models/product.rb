81f7709be342318f98b766c28661f0af32069f39
Georgy <geovo851@gmail.com>
2015-08-17 06:19:09 -0700

    Update 17_08

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :products_orders
   has_many :orders, through: :products_orders
-  # mount_uploader :photo, ImageUploader 
+  mount_uploader :photo, ImageUploader
+
+  validates :name, :category_id, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
